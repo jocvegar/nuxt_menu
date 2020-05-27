59f86ac73b6defde195dbd4dda9778d5e98becd9
Jose <jocvegar@gmail.com>
2020-05-27 14:26:31 -0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        nuxt_menu
+      </h1>
+      <h2 class="subtitle">
+        Siu Traditional Chinese Cuisine
+      </h2>
+      <h3>
+        test
+        <span>TEST ME</span>
+      </h3>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.container {
+  h3 {
+    color: green;
+    span {
+      background-color: tomato;
+      &:hover {
+        color:yellow;
+        cursor: wait;
+      }
+    }
+  }
+}
+</style>
